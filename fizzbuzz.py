cd5ed770cdb98f8e9b0f755ec0c7f1ed6101ad51
Seb <deviantseb@gmail.com>
2017-08-07 17:37:50 +0200

    Improved functionality

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,16 +2,21 @@
 # is either a multiple of 3, 5, or both (3 & 5), respectively,
 # for a range of numbers between 1 to 100
 
-x = 0
-for x in range(0,100):
+
+from_number = int(input("Range from?: "))
+to_number = int(input("Range to?: "))
+fizz_num = int(input("What is Fizz? "))
+buzz_num = int(input("What is Buzz? "))
+
+for x in range((from_number - 1), to_number):
 	x += 1
-	if (x / 15.0).is_integer():
+	if x % fizz_num == 0 and x % buzz_num == 0:
 		print("FizzBuzz")
 
-	elif (x / 3.0).is_integer():
+	elif x % 3 == 0:
 		print("Fizz")
 	
-	elif (x / 5.0).is_integer():
+	elif x % 5 == 0:
 		print("Buzz")
 
 	else:
